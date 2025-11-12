5ee963ee7147a583333fe0323ec1f62712c6289b
Adarsh261206 <adarsh2612006@gmail.com>
2025-11-13 00:37:05 +0530

    Remove (public) route group - fix Vercel tracing issue

--- app/page.tsx
+++ app/page.tsx
@@ -1,22 +1,5 @@
-'use client';
-
-import { useEffect } from 'react';
-import { useRouter } from 'next/navigation';
+import { redirect } from 'next/navigation';
 
 export default function HomePage() {
-  const router = useRouter();
-
-  useEffect(() => {
-    // Redirect to epaper on client side
-    router.push('/epaper');
-  }, [router]);
-
-  return (
-    <div className="min-h-screen flex items-center justify-center bg-gray-100">
-      <div className="text-center">
-        <div className="w-16 h-16 border-4 border-red-600 border-t-transparent rounded-full animate-spin mx-auto mb-4"></div>
-        <p className="text-gray-600">Loading...</p>
-      </div>
-    </div>
-  );
+  redirect('/epaper');
 }
